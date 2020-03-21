5fe7cb2655a8977f0754b28ac56f2d5a4b535e4a
marymuffmuncher <62108877+marymuffmuncher@users.noreply.github.com>
2020-03-21 17:55:17 +0100

    Update __init__.py

--- kaffelista/__init__.py
+++ kaffelista/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 
 
@@ -8,7 +9,10 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '42b91f0708b26ae05c592eb5c0fe075e'
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
-login_maganer = LoginManager(app)
 
 from kaffelista import routes
