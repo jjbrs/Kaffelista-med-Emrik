2814bd396c8d216264db908f708c19949f99ffdf
marymuffmuncher <62108877+marymuffmuncher@users.noreply.github.com>
2020-03-24 19:16:29 +0100

    Update addsometodatabase.py

--- addsometodatabase.py
+++ addsometodatabase.py
@@ -1,7 +1,6 @@
 import os
-from kaffelista import db
+from kaffelista import db, bcrypt
 from kaffelista.models import User, Purchase, Invoice, Fika
-
 
 try:
     os.remove('kaffelista/site.db')
@@ -11,25 +10,24 @@ except:
 
 db.create_all()
 
-fika_kaffe=Fika(name_of_fika='Kaffe', price=2)
-db.session.add(fika_kaffe)
+fika=Fika(name_of_fika='Kaffe', price=2)
+db.session.add(fika)
 
-fika_kaffe_m_mjolk=Fika(name_of_fika='Kaffe med mjölk', price=3)
-db.session.add(fika_kaffe_m_mjolk)
+fika=Fika(name_of_fika='Kaffe med mjölk', price=3)
+db.session.add(fika)
 
-fika_te=Fika(name_of_fika='Te', price=2)
-db.session.add(fika_te)
+fika=Fika(name_of_fika='Te', price=2)
+db.session.add(fika)
 
-fika_te_m_mjolk=Fika(name_of_fika='Te med mjölk', price=3)
-db.session.add(fika_te_m_mjolk)
+fika=Fika(name_of_fika='Te med mjölk', price=3)
+db.session.add(fika)
 
-fika_kaka=Fika(name_of_fika='Kaka', price=2)
-db.session.add(fika_kaka)
+fika=Fika(name_of_fika='Kaka', price=2)
+db.session.add(fika)
 
 
 hashed_password = bcrypt.generate_password_hash('password').decode('utf-8')
-default_user1=User(username='johannatest', first_name='johanna', last_name='souza',
-                   email='test@mail.com', password=hashed_password)
+default_user1=User(username='johannatest', first_name='johanna', last_name='souza', email='test@mail.com', password=hashed_password)
 db.session.add(default_user1)
 
 db.session.commit()
@@ -39,15 +37,10 @@ db.session.commit()
 
 
 
-
-
-
-
-
 #def createFika():
-#   name_of_fika = ["Kaffe", "Te", "Mjölk", "Kaka"]
-#   price = [2,2,1,2]
+  # name_of_fika = ['kaffe', 'kaffe+mjölk', 'te', 'te+mjölk', 'kaka']
+  # price = [2,3,2,3,2]
 
-#   for i in range(2):
-#       fika = Fika(name_of_fika=name_of_fika[i], price = price[i])
-#       db.session.add(fika)
+   #for i in range(2):
+    #   fika = Fika(name_of_fika=name_of_fika[i], price = price[i])
+     #  db.session.add(fika)
